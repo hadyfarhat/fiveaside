5623867c1917bc5febb974ba97946f9b8300fbd1
Hadi Farhat <hadi236a@gmail.com>
2017-10-07 13:10:18 +0100

    basic layout and style

--- fiveaside/fiveaside/urls.py
+++ fiveaside/fiveaside/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+# import general view
+from . import views
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    # home url
+    url(r'^$', views.Home.as_view(), name='home'),
 ]
